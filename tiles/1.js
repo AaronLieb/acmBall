ad1b1718765525c7f5de4edc8a5ff67cbc442bb6
AaronLieb <aarnliebrmn@gmail.com>
2022-04-11 12:48:22 -0700

    added tiles directory

---
+++ tiles/1.js
@@ -0,0 +1,33 @@
+var START = {
+  position: {
+    x: 0,
+    y: 290,
+  },
+  velocity: {
+    x: 3,
+    y: 0,
+  },
+};
+
+var END = {
+  position: {
+    x: 0,
+    y: 290,
+  },
+  velocity: {
+    x: 3,
+    y: 0,
+  },
+};
+
+function setupFunction(tile) {
+  tile.ground = Bodies.rectangle(
+    250,
+    tile.render.canvas.height,
+    tile.render.canvas.width,
+    5,
+    { isStatic: true }
+  );
+  tile.ground.render.fillStyle = "fff";
+  Composite.add(tile.engine.world, [tile.ground]);
+}
