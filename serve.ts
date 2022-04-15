e26eced98469f2a79a8f38f33c91f39ff6397b63
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-04-15 14:22:35 -0700

    setup deno

---
+++ serve.ts
@@ -0,0 +1,44 @@
+import { serve } from "http/server.ts";
+
+async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  // This is how the server works:
+  // 1. A request comes in for a specific asset.
+  // 2. We read the asset from the file system.
+  // 3. We send the asset back to the client.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    // Read the style.css file from the file system.
+    const file = await Deno.readFile("./style.css");
+    // Respond to the request with the style.css file.
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    },
+  );
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handleRequest);
+
+if (import.meta.main) {
+}
