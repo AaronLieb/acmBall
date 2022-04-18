8b0e45f746cddff27f1a26a853e7fe7079b83acf
AaronLieb <aarnliebrmn@gmail.com>
2022-04-18 11:32:26 -0700

    added test debug box

--- tests.js
+++ tests.js
@@ -6,7 +6,14 @@ const VELOCITY_DELTA = 0.2;
 
 export let assertEqual = (a, b, delta, msg) => {
   if (Math.abs(a - b) > delta) {
-    console.log(`[${msg}] TEST CASE FAILED ${a} != ${b} | delta = ${delta}`);
+    let fail = `[${msg}] TEST CASE FAILED ${
+      Math.round(a * 100) / 100
+    } != ${b} | delta = ${delta}`;
+    console.log(fail);
+    let ele = document.createElement("p");
+    ele.innerHTML = fail;
+    document.getElementById("testlogs").appendChild(ele);
+    document.getElementById("testbox").style.backgroundColor = "red";
     return false;
   }
   //console.log(`[${msg}] TEST CASE PASSED ${a} == ${b} | delta = ${delta}`);
@@ -16,11 +23,20 @@ export let assertEqual = (a, b, delta, msg) => {
 export let testExit = (ball, end) => {
   let flag = true;
   let est_pos = findIntersection("y", Game.ball);
-  let rel_est_pos = {x: ((est_pos.x - 1) % Game.TILE_WIDTH) + 1, y: ((est_pos.y - 1) % Game.TILE_HEIGHT) + 1}
-  flag = assertEqual(rel_est_pos.x, end.position.x, POSITION_DELTA, "Ball Position X") && flag;
-  flag = assertEqual(rel_est_pos.y, end.position.y, POSITION_DELTA, "Ball Position Y") && flag;
-  flag = assertEqual(ball.velocity.x, end.velocity.x, VELOCITY_DELTA, "Ball Velocity X") && flag;
-  flag = assertEqual(ball.velocity.y, end.velocity.y, VELOCITY_DELTA, "Ball Velocity Y") && flag;
+  let rel_est_pos = {
+    x: ((est_pos.x - 1) % Game.TILE_WIDTH) + 1,
+    y: ((est_pos.y - 1) % Game.TILE_HEIGHT) + 1,
+  };
+  flag =
+    assertEqual(rel_est_pos.x, end.position.x, POSITION_DELTA, "Ball Position X") && flag;
+  flag =
+    assertEqual(rel_est_pos.y, end.position.y, POSITION_DELTA, "Ball Position Y") && flag;
+  flag =
+    assertEqual(ball.velocity.x, end.velocity.x, VELOCITY_DELTA, "Ball Velocity X") &&
+    flag;
+  flag =
+    assertEqual(ball.velocity.y, end.velocity.y, VELOCITY_DELTA, "Ball Velocity Y") &&
+    flag;
   if (flag) console.log("ALL TESTS PASSED!");
   return flag;
 };
