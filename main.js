d0891874751a93bb56789890f2c3743fc2e0bca1
AaronLieb <aarnliebrmn@gmail.com>
2022-04-18 14:23:09 -0700

    updated todo comments

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 /* TODO 
     Add mouse to see coordinates, for easier placement
 
-    Write a custom renderer
-
+    Write a custom renderer:
+      pick beter names for color, border, width
+      make borders inside rather than outside
+      remove borders on same-type shapes that share verticies? (might be too hard on performance)
+    
+    Camera:
+      change canvas size easily
+      add smoothing algorithm
+      disable camera
+    
+    Game:
+      add start button
+    
     Create a spring composition, with constraints
 
     Look into the engine and see what cool options there are to wrap
 
+    Backend: 
+      Create CI infastructure
+      Only send successful test case attempts
 */
 
 import Game from "./Game.js";
