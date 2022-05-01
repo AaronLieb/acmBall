e930bfd159ff86c2c88137ec1323fa2996ec64ed
AaronLieb <aarnliebrmn@gmail.com>
2022-04-30 19:11:15 -0700

    changed config from modular js to json file, testResults now reads id from config.json

--- testResults.sh
+++ testResults.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Usage: ./testResults.sh <tileId>
+# Usage: ./testResults.sh
+
+id=$(jq ".tile_id" config.json)
 
 r=$(curl \
   -H 'X-Master-key: $2b$10$BfSdlGY7.T2MK8eNEgBqx.ZDgA9oto5l2NO6PogwTLk27MQeiWRpC' \
   -H 'X-Bin-Meta: false' \
   --request GET \
-    https://api.jsonbin.io/v3/b/626daf2125069545a32b655a/latest | jq ".results[$1]")
+    https://api.jsonbin.io/v3/b/626daf2125069545a32b655a/latest | jq ".results[$id]")
 
- [[ $r = "true" ]] && e=0 || e=1
- exit $e
+echo $e
+
+[[ $r = "true" ]] && e=0 || e=1
+
+exit $e
