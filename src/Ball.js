eeca5260bd0e3db60c9f6ee8d37900095ff43fd7
AaronLieb <aarnliebrmn@gmail.com>
2022-05-04 13:58:18 -0700

    moved constants outside of constructorJ

--- src/Ball.js
+++ src/Ball.js
@@ -1,8 +1,37 @@
+import Entity from "./Entity.js";
+
+const BALL_RADIUS = 40;
+
+const defaultState = {
+  frictionAir: 0,
+  friction: 0.0006,
+  restitution: 0.8,
+  inertia: Infinity,
+  inverseInertia: 0,
+  render: {
+    fillStyle: "#f99",
+    lineWidth: 5,
+    strokeStyle: "black",
+    visible: true,
+  },
+};
+
 /**
  * @class Ball
  */
-class Ball {
-  constructor() {
+class Ball extends Entity {
+  /**
+   *
+   * @param {Tile} tile - the tile you want the ball to start in
+   */
+  constructor(tile) {
+    let ball = Matter.Bodies.circle(0, 0, BALL_RADIUS, defaultState);
+    super(ball, tile, false);
+  }
 
+  resetState() {
+    Matter.Body.set(this.body, defaultState);
   }
 }
+
+export default Ball;
