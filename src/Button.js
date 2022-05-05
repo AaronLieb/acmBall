8f751290c5d6f4120dcf187a173401bacd1169bd
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-05-05 01:35:57 -0700

    🔧 here ya go

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,4 @@
+import Entity from "./Entity.js";
 import Rectangle from "./Rectangle.js";
 
 /**
@@ -8,6 +9,13 @@ class Button extends Rectangle {
   constructor(tile, x, y, width, height, startCollide, endCollide, options) {
     super(tile, x, y, width, height, !(options?.isStatic ?? true), options);
 
+    let sensorBody = Matter.Bodies.rectangle(x, y, width+1, height+100, {isStatic: true, isSensor: true});
+    let sensor = new Entity(sensorBody, tile, false, false);
+    sensor.color = 'rgba(42, 42, 42, 0.4)';
+    sensor.body.label = 'sensor'
+    // sensor.body.parent = this.body;
+    Matter.Body.setParts(this.body, [...this.body.parts,...sensor.body.parts], true)
+    console.log(this.body.parts);
     this.unpressedColor = options.unpressedColor ?? "red";
     this.pressedColor = options.pressedColor ?? "green";
 
--- tiles/0.js
+++ tiles/0.js
@@ -4,7 +4,7 @@ import Tile from "../src/Tile.js";
 let tile = new Tile();
 
 tile.ballStart.position = { x: 0, y: 0 };
-tile.ballStart.velocity = { x: 5, y: 0 };
+tile.ballStart.velocity = { x: 1, y: 0 };
 
 tile.ballEnd.position = { x: 0, y: 0 };
 tile.ballEnd.velocity = { x: 0, y: 0 };
@@ -16,9 +16,7 @@ tile.setup = function () {
   tile.createRectangle(tile.width / 2, tile.height - 20, tile.width, 40);
   let r = tile.createRectangle(100, 100, 50, 50, true);
   r.velocity = { x: 20, y: 0 };
-  t = tile.createConveyorBelt(300, 375, 200, 50, 15);
-  // t = tile.createButton(300, 375, 200, 50, ()=>{console.log('start button')});
-  t.angle = -70;
+  t = tile.createButton(100, 425, 200, 50, 15);
 };
 
 // This function will run when the ball enters your tile
@@ -27,7 +25,7 @@ tile.onBallEnter = async function () {};
 tile.onBallLeave = async function () {};
 
 // This function will run once every tick while the ball is in your tile
-tile.onTick = function () {};
+tile.onTick = function () {t.angle += 1.5;};
 
 // This function will run once everey tick, even if the ball is not in your tile
 tile.onTickBackground = function () {};
