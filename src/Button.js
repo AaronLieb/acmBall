cee9ff9953d96152cb0820fbf3c0e36ff8d05c6c
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-05-05 15:22:59 -0700

    🔧 added ropes and spring compress

--- src/Button.js
+++ src/Button.js
@@ -19,34 +19,37 @@ class Button extends Rectangle {
     this.pressedColor = options.pressedColor ?? "green";
     this.body.label = 'buttonBase'
     this.color = this.unpressedColor;
-    this.ballOnly = options.ballOnly ?? false;
+    sensor.body.ballOnly = options.ballOnly ?? false;
     sensor.body.callback = (o) => {this.color = this.pressedColor; startCollide(o);};
     sensor.body.endCallback = (o) => {this.color = this.unpressedColor; endCollide(o);};
     
   }
 
   static buttonLogic(a, b, event) {
+    console.log(a.label, b.label, event);
     if (a.callback && event.name === "collisionStart") {
       if (a.ballOnly && b.label === "ball") {
-        a.callback();
+        
+        a.callback(b);
       } else if (!a.ballOnly) a.callback(b);
     }
-
+    
     if (a.endCallback && event.name === "collisionEnd") {
       if (a.ballOnly && b.label === "ball") {
-        a.endCallback();
+        a.endCallback(b);
       } else if (!a.ballOnly) a.endCallback(b);
     }
-
+    
     if (b.callback && event.name === "collisionStart") {
       if (b.ballOnly && a.label === "ball") {
-        b.callback();
+        console.log('inside');
+        b.callback(a);
       } else if (!b.ballOnly) b.callback(a);
     }
 
     if (b.endCallback && event.name === "collisionEnd") {
       if (b.ballOnly && a.label === "ball") {
-        b.endCallback();
+        b.endCallback(a);
       } else if (!b.ballOnly) b.endCallback(a);
     }
   }
