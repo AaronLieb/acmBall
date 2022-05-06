bc8f2761cb6cbce0de481e8be3ceee73f2473a5e
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-06 03:53:57 -0700

    fixed justins bad commit

--- src/helpers.js
+++ src/helpers.js
@@ -7,16 +7,16 @@ export const unitVector = (vec) => {
   return { x: vec.x / mag, y: vec.y / mag };
 };
 
-export const findIntersection = (axis, ball) => {
-  // TODO improve this math and variable names
-  if (!(axis == "y" || axis == "x")) return;
+export const findIntersection = (ball) => {
+  let pos = game.ball.position;
+  let axis = pos.x >= game.TILE_WIDTH || pos.x <= 0 ? "y" : "x";
   let opposite = axis == "x" ? "y" : "x";
-  let target = Math.round(ball.position[opposite] / 100) * 100;
-  let d = target - ball.position[opposite];
+  let target = 500;
+  let d = target - pos[opposite];
   let unit_vector = unitVector(ball.velocity);
   let ratio = d / unit_vector[opposite];
-  let estimated_axis = ball.position[axis] + ratio * unit_vector[axis];
-  if (ball.velocity[axis] < 0 && ball.velocity[axis] > -0.5) estimated_axis = ball.position[axis];
+  let estimated_axis = pos[axis] + ratio * unit_vector[axis];
+  if (ball.velocity[axis] < 0 && ball.velocity[axis] > -0.5) estimated_axis = pos[axis];
   let estimated_pos = {};
   estimated_pos[opposite] = target;
   estimated_pos[axis] = estimated_axis;
