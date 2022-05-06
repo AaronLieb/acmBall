256a0db8d8e9e1fa3578631b522c56e5ff985a37
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-05-06 14:41:37 -0700

    🔧 fix ramp

--- src/Ramp.js
+++ src/Ramp.js
@@ -2,8 +2,10 @@ import Triangle from "./Triangle.js";
 
 class Ramp extends Triangle {
   constructor(tile, x1, y1, x2, y2, options = {}) {
-    let { x3, y3 } = y2 > y1 ? { x3: x1, y3: y2 } : { x3: x2, y3: y2 };
-    super(tile, x1, y1, x3, y3, x2, y2, false, options);
+    // let { x3, y3 } = y2 > y1 ? { x3: x1, y3: y2 } : { x3: x2, y3: y2 };
+    let higher = (y1 > y2) ? y1 : y2;
+    let base = (y1 < y2) ? {x: x1, y: y2} : {x: x2, y: y1};
+    super(tile, x1, y1, base.x, base.y, x2, y2, false, options);
     this.body.label = "ramp";
   }
 }
