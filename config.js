dd0fd6511376b39dcf55bca0e67b8c0dd00c88de
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-05 20:17:13 -0700

    forced config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const config = {
+  tile_id: 0,
+  tests: {
+    exit: {
+      position: true,
+      velocity: true,
+      size: true,
+      shape: true,
+      render: true,
+    },
+  },
+  debug: {
+    showMasses: false,
+    showTimer: true,
+    showMousePosition: true,
+    showTileBorder: true,
+  },
+  logging: {
+    matter: 0,
+  },
+};
+
+export default config;
