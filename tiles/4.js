78061dd324fa19e7b05e0381437b4606c967197c
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-05-06 15:14:13 -0700

    🔧 tiles 4-7

--- tiles/4.js
+++ tiles/4.js
@@ -3,15 +3,19 @@ import Tile from "../src/Tile.js";
 
 let tile = new Tile();
 
-tile.ballStart.position = { x: 0, y: 0 };
-tile.ballStart.velocity = { x: 0, y: 0 };
+tile.ballStart.position = { x: 500, y: 411.041 };
+tile.ballStart.velocity = { x: -7.390, y: -1.271 };
 
-tile.ballEnd.position = { x: 0, y: 0 };
+//(DEBUG) Tile: 5 , Ball Exited at: (0.000, 411.041) with velocity: (-7.390, -1.271) and entered Tile: 4
+//
+tile.ballEnd.position = { x: 62, y: 500 };
 tile.ballEnd.velocity = { x: 0, y: 0 };
 
 // This function will run once when the tile loads for the first time
 tile.setup = function () {
-  tile.createRectangle(tile.width / 2, tile.height - 20, tile.width, 40);
+  // tile.createRectangle(tile.width / 2, tile.height - 20, tile.width, 40);
+  let portal = tile.createPortals(402, 400, 200, 200);
+  let spring = tile.createSpring(12, 400, 50, 300, 0, 0);
 };
 
 // This function will run when the ball enters your tile
--- tiles/5.js
+++ tiles/5.js
@@ -3,11 +3,15 @@ import Tile from "../src/Tile.js";
 
 let tile = new Tile();
 
-tile.ballStart.position = { x: 0, y: 0 };
-tile.ballStart.velocity = { x: 0, y: 0 };
+tile.ballStart.position = { x: 500, y: 366.4 };
+tile.ballStart.velocity = { x: -7.390, y: 1.782};
 
-tile.ballEnd.position = { x: 0, y: 0 };
-tile.ballEnd.velocity = { x: 0, y: 0 };
+//(DEBUG) Tile: 6 , Ball Exited at: (0.000, 366.385) with velocity: (-7.390, 1.782) and entered Tile: 5
+
+
+//(DEBUG) Tile: 5 , Ball Exited at: (0.000, 411.041) with velocity: (-7.390, -1.271) and entered Tile: 4
+tile.ballEnd.position = { x: 0, y: 411.041 };
+tile.ballEnd.velocity = { x: -7.390, y: -1.271 };
 
 // This function will run once when the tile loads for the first time
 tile.setup = function () {
