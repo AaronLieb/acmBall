65b96e673facef2978dbcd404ab9236cd4d06a91
Jorge <jorgeemejia@csu.fullerton.edu>
2022-05-06 17:28:23 -0700

    Level : Conveyor belt only

--- tiles/10.js
+++ tiles/10.js
@@ -5,7 +5,7 @@ let tile = new Tile();
 
 tile.ballStart.position = { x: 0, y: 127.28 };
 tile.ballStart.velocity = { x: 10, y: -15.56 };
-
+ 
 tile.ballEnd.position = { x: 500, y: 60};
 tile.ballEnd.velocity = { x: 3, y: 0};
 
@@ -16,8 +16,10 @@ tile.setup = function () {
   let smolBump = tile.createRectangle(68, 10, 20, groundT);
   smolBump.angle -= 0;
 
-  let platform = tile.createRectangle(350, 450, 40, groundT);
-  platform.angle += 25;
+  let platform = tile.createConveyorBelt(350,105,500,50,3)
+
+  // let platform = tile.createRectangle(350, 450, 40, groundT);
+  // platform.angle += 25;
 
   let  ramp =  tile.createRamp(110,122,493,436);
 };
